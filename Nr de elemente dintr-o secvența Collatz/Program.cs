f112c3d6626f198f9d1c08653ec66476d523b4d8
Harkai Iosif <harkai222@gmail.com>
2022-10-17 17:14:49 +0300

    Add project files.

---
+++ Nr de elemente dintr-o secvența Collatz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nr_de_elemente_dintr_o_secvența_Collatz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nr = 1;
+            while (n != 1)
+            {
+                nr = nr + 1;
+                if (n % 2 == 0) n = n / 2;
+                else n = 3 * n + 1;
+
+            }
+            Console.WriteLine(nr);
+            Console.ReadKey();
+        }
+    }
+}
